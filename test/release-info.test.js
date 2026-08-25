2f587d6facd8a82b8a0a5051eebb5988f7e61014
giledwardsmusic-byte <giledwardsmusic@gmail.com>
2026-08-25 17:59:46 -0500

    Gate release test on all completion safeguards

--- test/release-info.test.js
+++ test/release-info.test.js
@@ -13,7 +13,8 @@ test('release manifest matches the static deployment marker', async()=>{
 test('release manifest advertises required completion safeguards',()=>{
   for(const key of [
     'instagram_from_facebook','pinterest_oauth','tiktok_oauth','google_drive_sync',
-    'marketing_copy_bank','paid_sale_email_alerts','unresolved_health_email_alerts',
+    'marketing_copy_bank','drive_media_ingestion','tracked_campaign_links','payhip_sales_attribution',
+    'paid_sale_email_alerts','unresolved_health_email_alerts',
     'media_normalization','media_quota_fallback','invalid_media_fail_closed'
   ]) assert.equal(RELEASE_CAPABILITIES[key],true,key);
 });
