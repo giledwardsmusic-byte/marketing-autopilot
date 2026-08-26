d4418409baa7c69dfa5007e8cb222a39363b3536
giledwardsmusic-byte <giledwardsmusic@gmail.com>
2026-08-26 00:57:09 -0500

    Test product-scoping and persistent-media release gates

--- test/release-info.test.js
+++ test/release-info.test.js
@@ -13,8 +13,9 @@ test('release manifest matches the static deployment marker', async()=>{
 test('release manifest advertises required completion safeguards',()=>{
   for(const key of [
     'instagram_from_facebook','pinterest_oauth','tiktok_oauth','google_drive_sync',
-    'marketing_copy_bank','drive_media_ingestion','tracked_campaign_links','payhip_sales_attribution',
+    'marketing_copy_bank','drive_media_ingestion','drive_creative_product_scoping',
+    'tracked_campaign_links','payhip_sales_attribution',
     'paid_sale_email_alerts','unresolved_health_email_alerts',
-    'media_normalization','media_quota_fallback','invalid_media_fail_closed'
+    'media_normalization','persistent_derived_media','media_quota_fallback','invalid_media_fail_closed'
   ]) assert.equal(RELEASE_CAPABILITIES[key],true,key);
 });
